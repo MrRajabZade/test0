137ea554d98cea9cdf60efd3518487bd7ca0a136
Mr RajabZade <146377979+MrRajabZade@users.noreply.github.com>
2025-08-18 09:07:51 +0330

    Create supabase_client.py

---
+++ my-api/supabase_client.py
@@ -0,0 +1,9 @@
+from supabase import create_client
+import os
+
+url = os.environ.get('SUPABASE_URL')
+key = os.environ.get('SUPABASE_KEY')
+supabase = create_client(url, key)
+
+def save_to_db(data):
+    return supabase.table('user_verifications').insert(data).execute()
