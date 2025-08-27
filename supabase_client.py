302d1b3ca9c858dba22c9e5e3bf2c02fc762982e
Mr RajabZade <146377979+MrRajabZade@users.noreply.github.com>
2025-08-27 08:58:54 +0330

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -4,10 +4,4 @@ import os
 url = os.environ.get('SUPABASE_URL')
 key = os.environ.get('SUPABASE_KEY')
 
-# اضافه کردن خط زیر برای دیباگ
-print(f"Connecting to Supabase at: {url}")  # این خط را اضافه کنید
-
 supabase = create_client(url, key)
-
-def save_to_db(data):
-    return supabase.table('user_verifications').insert(data).execute()
